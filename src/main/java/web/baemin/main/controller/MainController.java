5bfb23da3d5ee5a3d1365fce5ae7d579123e0e3b
qtm <zmxnxnxn1@naver.com>
2022-12-07 17:46:33 +0900

    스토어 API

--- src/main/java/web/baemin/main/controller/MainController.java
+++ src/main/java/web/baemin/main/controller/MainController.java
@@ -27,4 +27,8 @@ public class MainController {
         model.addAttribute("storeTypeList", storeTypeList);
     }
 
+    @GetMapping("/storeApi")
+    public void storeApi(Model model) {
+    }
+
 }
